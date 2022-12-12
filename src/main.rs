18c23d8851ae4727d84bdf7a5f21bd540b882f9a
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-12 17:05:09 -0300

    2st stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use std::{net::TcpListener, io::Result};
+use std::{net::{TcpListener, TcpStream}, io::{Result, Read, Write}};
 
 
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379")?;
-    match listener.accept() {
-        Ok((_socket, addr)) => println!("new client: {addr:?}"),
-        Err(e) => println!("couldn't get client: {e:?}"),
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_request(stream);
+    }
+
+    Ok(())
+}
+
+fn handle_request(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let ping = b"$4\r\nping\r\n";
+
+    stream.read(&mut buffer).unwrap();
+
+    if buffer.starts_with(ping) {
+        stream.write(b"+PONG\r\n").unwrap();
+        stream.flush().unwrap();
     }
     
-    Ok(())
 }
